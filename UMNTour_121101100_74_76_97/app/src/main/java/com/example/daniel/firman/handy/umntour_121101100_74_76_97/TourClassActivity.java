725405256d8ccde1c3d62aec29ed63b6f99a5a04
Handy Wijaya <handy.wijaya.p@gmail.com>
2015-11-29 01:23:37 +0700

    - pembuatan template

---
+++ UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/TourClassActivity.java
@@ -0,0 +1,65 @@
+package com.example.daniel.firman.handy.umntour_121101100_74_76_97;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by Handy on 11/28/2015.
+ */
+public class TourClassActivity extends AppCompatActivity {
+    Boolean isFabOpen = false;
+    FloatingActionButton fab,fab1,fab2,fab3;
+    Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    ImageView bg;
+    Intent i;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void getButtonsFromXml() {
+        fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+    }
+
+    public void animateFab() {
+        if(isFabOpen){
+
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab3.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            fab3.setClickable(false);
+            isFabOpen = false;
+
+        } else {
+
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab3.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            fab3.setClickable(true);
+            isFabOpen = true;
+            Log.d("Raj", "open");
+
+        }
+    }
+}
