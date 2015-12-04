68d8b556da2e2d21b3fa0c79774ddaf7f7cd4943
cerrion <danny.vgo@gmail.com>
2015-12-04 20:27:26 +0700

    lantai 8 selesai

---
+++ UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai8/Lantai_8_1.java
@@ -0,0 +1,68 @@
+package com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.R;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.TourClassActivity;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.external.BlurDialog;
+
+public class Lantai_8_1 extends TourClassActivity {
+
+    public ImageButton ivStairsDown, ivMoveForward, ivMoveBackward, ivMoveToilet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lantai_8_1);
+
+        floorInformation = 8;
+        String information = "Consists of normal and big classrooms";
+        setUpLayout(information);
+
+        ivStairsDown = (ImageButton) findViewById(R.id.ivStairDown);
+        ivStairsDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info = "Do you want to go downstairs?";
+                blurDialog = new BlurDialog().newInstance(R.layout.dialog_fragment_change_floor, Lantai_8_1.this, floorInformation + 1, info);
+                blurDialog.show(getFragmentManager(), "blur_sample");
+            }
+        });
+
+        ivMoveForward = (ImageButton) findViewById(R.id.ivMoveForward);
+        ivMoveForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_8_3.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleInBegin(v);
+            }
+        });
+
+        ivMoveBackward = (ImageButton) findViewById(R.id.ivMoveBackward);
+        ivMoveBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_8_8.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleOutBegin(v);
+            }
+        });
+
+        ivMoveToilet = (ImageButton) findViewById(R.id.ivMoveToilet);
+        ivMoveToilet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_8_2.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleInBegin(v);
+            }
+        });
+
+        animateFadeInBegin();
+    }
+
+}
---
+++ UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai9/Lantai_9_1.java
@@ -0,0 +1,16 @@
+package com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai9;
+
+import android.os.Bundle;
+
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.R;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.TourClassActivity;
+
+public class Lantai_9_1 extends TourClassActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lantai_9_1);
+    }
+
+}
