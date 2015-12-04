d35f08547124644a95d08e354dbb14255601229b
cerrion <danny.vgo@gmail.com>
2015-12-04 13:15:24 +0700

    lantai 8 selesai

---
+++ UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai8/Lantai_8_4.java
@@ -0,0 +1,70 @@
+package com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.R;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.TourClassActivity;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.external.BlurDialog;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.kelas.KelasBesarTengah;
+
+public class Lantai_8_4 extends TourClassActivity {
+
+    public ImageButton ivMoveForward, ivMoveBackward, ivEnter813, ivEnter814;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lantai_8_4);
+
+        floorInformation = 8;
+        String information = "Consists of normal and big classrooms";
+        setUpLayout(information);
+
+        ivMoveForward = (ImageButton) findViewById(R.id.ivMoveForward);
+        ivMoveForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_8_5.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleInBegin(v);
+            }
+        });
+
+        ivMoveBackward = (ImageButton) findViewById(R.id.ivMoveBackward);
+        ivMoveBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_8_3.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleOutBegin(v);
+            }
+        });
+
+        ivEnter813 = (ImageButton) findViewById(R.id.ivEnter813);
+        ivEnter813.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), KelasBesarTengah.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleInBegin(v);
+            }
+        });
+
+        ivEnter814 = (ImageButton) findViewById(R.id.ivEnter814);
+        ivEnter814.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info = "C814\nBig Classroom";
+                blurDialog = new BlurDialog().newInstance(R.layout.dialog_fragment_information, Lantai_8_4.this, floorInformation, info);
+                blurDialog.show(getFragmentManager(), "blur_sample");
+            }
+        });
+
+        animateFadeInBegin();
+    }
+
+}
