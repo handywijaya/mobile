f78771ca0f908d4df2bfa75efdb3b4e75c8d79ed
Firman <firmansyah.ramadhan01@gmail.com>
2015-12-04 13:33:21 +0700

    - ya gitu

--- UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai7/Lantai_7_1.java
+++ UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai7/Lantai_7_1.java
@@ -1,17 +1,92 @@
 package com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.daniel.firman.handy.umntour_121101100_74_76_97.R;
 import com.example.daniel.firman.handy.umntour_121101100_74_76_97.TourClassActivity;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.external.BlurDialog;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai6.Lantai_6_2;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai6.Lantai_6_3;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.lantai6.Lantai_6_8;
 
 public class Lantai_7_1 extends TourClassActivity {
+
+    public ImageButton ivStairsDown, ivStairsUp, ivMoveForward, ivMoveBackward, ivMoveToilet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lantai_7_1);
+        //lantai berapa
+        floorInformation = 7;
+
+        //informasi yang mau dimasukkan
+        String information = "Consists of Big Classrooms";
+
+        setUpLayout(information);
+
+        ivStairsDown = (ImageButton) findViewById(R.id.ivStairDown);
+        ivStairsDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info = "This stairs connects the 7th floor and the 6th floor of tower C";
+
+                //kalo turun kasi floorInformation - 1
+                blurDialog = new BlurDialog().newInstance(R.layout.dialog_fragment_other_floor, Lantai_7_1.this, floorInformation - 1, info);
+                blurDialog.show(getFragmentManager(), "blur_sample");
+            }
+        });
+
+        ivStairsUp = (ImageButton) findViewById(R.id.ivStairUp);
+        ivStairsUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //keterangan dialog fragment
+                String info = "Do you want to go upstairs?";
+
+                //kalo naek kasi floorInformation + 1
+                blurDialog = new BlurDialog().newInstance(R.layout.dialog_fragment_change_floor, Lantai_7_1.this, floorInformation + 1, info);
+                blurDialog.show(getFragmentManager(), "blur_sample");
+            }
+        });
+
+        //selain backward pake scale in
+        ivMoveForward = (ImageButton) findViewById(R.id.ivMoveForward);
+        ivMoveForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_6_3.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleInBegin(v);
+            }
+        });
+
+        //backward pake scale out
+        ivMoveBackward = (ImageButton) findViewById(R.id.ivMoveBackward);
+        ivMoveBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_6_8.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleOutBegin(v);
+            }
+        });
+
+        ivMoveToilet = (ImageButton) findViewById(R.id.ivMoveToilet);
+        ivMoveToilet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(getApplicationContext(), Lantai_6_2.class);
+                animateFadeOutButtonBegin(layout);
+                animateScaleInBegin(v);
+            }
+        });
+
+        animateFadeInBegin();
     }
 
 }
