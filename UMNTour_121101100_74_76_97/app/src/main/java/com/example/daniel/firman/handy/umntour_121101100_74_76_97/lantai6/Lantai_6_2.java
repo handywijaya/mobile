3e1e93fdcf011680f80c7963ab9590bc9fa756bc
Handy Wijaya <handy.wijaya.p@gmail.com>
2015-12-10 22:54:30 +0700

    - tambah help toilet

--- UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai6/Lantai_6_2.java
+++ UMNTour_121101100_74_76_97/app/src/main/java/com/example/daniel/firman/handy/umntour_121101100_74_76_97/lantai6/Lantai_6_2.java
@@ -8,10 +8,11 @@ import android.widget.ImageButton;
 
 import com.example.daniel.firman.handy.umntour_121101100_74_76_97.R;
 import com.example.daniel.firman.handy.umntour_121101100_74_76_97.TourClassActivity;
+import com.example.daniel.firman.handy.umntour_121101100_74_76_97.external.BlurDialog;
 
 public class Lantai_6_2 extends TourClassActivity {
 
-    ImageButton ivMoveBackward;
+    ImageButton ivMoveBackward, ivMoveToilet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,16 @@ public class Lantai_6_2 extends TourClassActivity {
                 i = new Intent(getApplicationContext(), Lantai_6_1.class);
                 animateFadeOutButtonBegin(layout);
                 animateScaleOutBegin(v);
+            }
+        });
+
+        ivMoveToilet = (ImageButton) findViewById(R.id.ivHelpToilet);
+        ivMoveToilet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String info = "Toilet\nThis door leads to male toilet, the door accross this door leads to female toilet";
+                blurDialog = new BlurDialog().newInstance(R.layout.dialog_fragment_information, Lantai_6_2.this, floorInformation, info);
+                blurDialog.show(getFragmentManager(), "blur_sample");
             }
         });
 
